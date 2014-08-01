19745ef1af4ab95e475bb4c5868e33f5051ff031
debyta <debyta.mm@gmail.com>
2014-08-01 16:29:11 +0200

    Inicios

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<header>
+	<div class="barra-sup">
+		<nav class="header-rrss">
+		<ul>
+			<a href="https://www.facebook.com/pages/Moner%C3%ADas/128569933881762" target="”_blank”/">
+			<li id="rrss-facebook" title="Facebook"></li>
+			</a>
+			<a href="https://twitter.com/PaMonisimaYo" target="”_blank”/">
+			<li id="rrss-twitter" title="Twitter"></li></a>
+			<a href="https://plus.google.com/102473913791150013806" target="”_blank”/">
+			    <li id="rrss-gp" title="Google+"></li>
+			</a>
+			<a href="http://www.bloglovin.com/en/blog/4787865" target="”_blank”/">
+			   <li id="rrss-bl" title="Bloglovin"></li>
+			</a>
+			<a href="http://instagram.com/pamonisimayo" target="”_blank”/">
+				<li id="rrss-instagram" title="Instagram"></li>
+			</a>
+			<a href="http://pinterest.com/pamonisimayo/" target="”_blank”/">
+				<li id="rrss-pinterest" title="Pinterest"></li>
+			</a>
+			<a href="https://www.youtube.com/channel/UCLenXZ8o8O-xeJfPWBHEMow" target="”_blank”/">
+				<li id="rrss-youtube" title="Youtube"></li>
+			</a>
+			<a href="">
+				<li id="rrss-contacto" title="Contacta conmigo"></li>
+			</a>
+		
+		</ul>
+		<ul>
+			<li>
+				<div class="search-nav">
+			<form action='/search' id='search' method='get' name='searchForm' style='display:inline;'> 
+			<input id='search-box' name='q' onblur='if (this.value == &quot;&quot;) this.value = &quot;Buscar...&quot;;' onfocus='if (this.value == &quot;Buscar...&quot;) this.value = &quot;&quot;;' size='28' type='text' value='Buscar...'/></form>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</nav>
+		
+	<div class="cabecera">
+		<img src="img/cabecera.png" alt="Pa Monisima Yo">
+	</div>
+
+</header>
+</body>
+</html>
---
+++ page-inicio.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pagina principal</title>
+</head>
+<body>
+	<div class=></div>
+</body>
+</html>
